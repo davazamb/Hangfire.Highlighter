f165e051b9680c4f156cda71fac3219343b181e4
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-06-06 18:00:57 -0400

    Instalado en nugget Hanfire update de owin, y creado el startup

---
+++ Hangfire.Highlighter/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Hangfire.Highlighter.Startup))]
+
+namespace Hangfire.Highlighter
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("HighlighterDb");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
