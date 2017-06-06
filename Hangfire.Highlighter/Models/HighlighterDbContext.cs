5719bfa3bba1fef90da80a9d9b074ff70b5ff843
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-06-06 16:46:51 -0400

    Creado Modelo DBcontext y cadena de conexion

---
+++ Hangfire.Highlighter/Models/HighlighterDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Hangfire.Highlighter.Models
+{
+    public class HighlighterDbContext : DbContext
+    {
+        public HighlighterDbContext() : base("HighlighterDb")
+        {
+        }
+    }
+}
